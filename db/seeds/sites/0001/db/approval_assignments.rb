f9dfe70cb0337060d24aea5dd4bbc839d36152dc
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-08-10 15:35:13 +0900

    Fix seeds

--- db/seeds/sites/0001/db/approval_assignments.rb
+++ db/seeds/sites/0001/db/approval_assignments.rb
@@ -2,13 +2,6 @@
 Approval::Assignment.create!(
   assignable_id: @approval_approvals_1.id,
   assignable_type: "Approval::Approval",
-  or_group_id: 0,
-  group_id: @sys_groups_0.id,
-  assign_type: "group_users"
-)
-Approval::Assignment.create!(
-  assignable_id: @approval_approvals_1.id,
-  assignable_type: "Approval::Approval",
   user_id: @sys_users_7.id,
   or_group_id: 0,
   assign_type: "user"
